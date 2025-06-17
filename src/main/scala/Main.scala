247eb3bfaf53a2d374e5ca41def19dd262d5d46f
FL4TL1NER <major_fl4tl1ner@outlook.com>
2025-06-18 02:03:52 +0300

    hello world (kinda)

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,12 +5,13 @@ import interpreter.Interpreter.{executeChunk, emptyInt}
 @main
 def main() = {
     val test = "" +
-        "a, b = 20, 45\n" +
-        "while a ~= b do if a>b then a = a-b else b=b-a end end\n" +
-        "return a"
+        "hello, world = \"Hello\", \"world\"\n" +
+        "return hello..\" \"..world"
     
+    println()
     val tokens = tokenize(test)
     parse(tokens) match
         case Left(value) => print(value)
         case Right(chunk) => print(executeChunk(chunk).runA(emptyInt))
+    println()
 }
